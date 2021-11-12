d5a4b8b857c9b537d7f6d7fbf1ca6a9201871ed1
Andre Summers <asummers@student.neumont.edu>
2021-11-11 18:33:27 -0700

    Update cosmosDBTest.js
    using variable

--- backend/cosmos/cosmosDBTest.js
+++ backend/cosmos/cosmosDBTest.js
@@ -1,4 +1,8 @@
+const cosmosPort = 10255;
+const dbName = 'totally-caring-web-service';
+const key = 'F4UX3UzubsvmqsgDt2ktYZZixdVtiXo5xf3TiUJulRvy8Qhts5wHM0SouYxvLW1jlyaAfccab4ox6A6e9bsSEg==';
+
 var mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://totally-caring-web-service:F4UX3UzubsvmqsgDt2ktYZZixdVtiXo5xf3TiUJulRvy8Qhts5wHM0SouYxvLW1jlyaAfccab4ox6A6e9bsSEg==@totally-caring-web-service.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@totally-caring-web-service@", function (err, db) {
+mongoClient.connect(`mongodb://${dbName}:${key}==@totally-caring-web-service.mongo.cosmos.azure.com:${cosmosPort}/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@totally-caring-web-service@`, function (err, db) {
   db.close();
 });
