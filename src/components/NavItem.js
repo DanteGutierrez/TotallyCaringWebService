6f5171648061a110977b0e1500ae0f33a41d1abb
and-sum-n <79877339+and-sum-n@users.noreply.github.com>
2021-11-22 18:43:52 -0700

    Navigation Bar

---
+++ src/components/NavItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+    const NavIcon = styled.div`
+`;
+
+const StyledNavItem = styled.div`
+  height: 70px;
+  width: 75px; /* width must be same size as NavBar to center */
+  text-align: center; /* Aligns <a> inside of NavIcon div */
+  margin-bottom: 0;   /* Puts space between NavItems */
+  a {
+    font-size: 2.7em;
+    color: ${(props) => props.active ? "white" : "#4C88E7"};
+    :hover {
+      opacity: 0.9;
+      text-decoration: none; /* Gets rid of underlining of icons */
+    }  
+  }
+`;
+
+class NavItem extends React.Component {
+
+    handleClick = () => {
+        const { path, onItemClick } = this.props;
+        onItemClick(path);
+      }
+    render() {
+        const { active } = this.props;
+      return (
+        <StyledNavItem active={active}>
+        <Link to={this.props.path} className={this.props.css} onClick={this.handleClick}>
+          <NavIcon></NavIcon>
+        </Link>
+      </StyledNavItem>
+      );
+    }
+  }
